6a0c864f8f9524049f89c956b85785ec726bbf26
Nenad Natosevic <nenad.natosevic@eon.com>
2016-09-28 11:25:55 +0200

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // -------------------------------------------------------------
+    // Countdown
+    // -------------------------------------------------------------
+    (function () {
+
+        $("#back-countdiown").countdown({
+            date: "7 october 2016 19:00:00",
+            format: "on"
+        });
+    
+    }()); 
+
+
+}); // JQuery end
+
+
+$(document).on('click', '.m-menu .dropdown-menu', function(e) {
+  e.stopPropagation()
+})
